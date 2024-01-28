2d461772b66a08557a0f791e3e5895a50f983950
lilcryptotech <lilcryptotech@gmail.com>
2024-01-28 12:13:33 -0500

    general text changes

---
+++ readme.md
@@ -0,0 +1,25 @@
+## wordago
+
+A word list generator  that help you generate a list of words
+line by line
+    
+
+
+
+
+
+ This program can also write files with words on them 
+
+
+
+
+
+
+
+## 
+
+
+
+
+
+
--- word.go
+++ word.go
@@ -1,8 +1,24 @@
-package wordago
-import "fmt"
+package main
+
+import (
+	"fmt"
+//	"math/rand"
+)
 func main(){
+var com string
+println("chose a command: ")
+fmt.Scanln(&com)
+if com == " " {
+lettter_low := "abcdefghijklmnopqrstuvwxyz"
+letter_cap := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers := "0123456789"
+symbols := "!@#$%&'()*+,-./"
 
 
 
 
 }
+
+}
+
+
