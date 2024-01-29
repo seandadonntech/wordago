f4c8fc947042fde84fd80a790914dcbb178bf1c5
lilcryptotech <lilcryptotech@gmail.com>
2024-01-29 08:28:55 -0500

    done adding the code

--- readme.md
+++ readme.md
@@ -1,22 +1,37 @@
-## wordago
+# Random Word Generator
 
-A word list generator  that help you generate a list of words
-line by line
-    
+This command-line application generates random words based on user preferences and allows you to save them to a file.
 
+## Table of Contents
 
+- [Introduction](#introduction)
+- [Usage](#usage)
+- [Requirements](#requirements)
+- [How to Run](#how-to-run)
+- [License](#license)
 
+## Introduction
 
+The Random Word Generator is a simple Go program that generates random words with options to include uppercase letters, lowercase letters, numbers, and symbols. It provides a flexible way to customize the generated words based on your preferences.
 
- ## Features
- This program can also write files with words on them 
+## Usage
 
-simply type how many words you want to generate
- 
+1. Run the program using the command: `go run main.go`.
+2. Enter the number of words you want to generate.
+3. Answer the prompts to include uppercase letters, lowercase letters, numbers, and symbols.
+4. Choose whether to create a file to store the generated words.
+5. The program will display the generated words or create a file named `generated_words.txt` with the generated words.
 
+## Requirements
 
+Make sure you have Go (Golang) installed on your system.
 
+## How to Run
 
+```bash
+go run word.go
+
+go build word.go
 
 
 
--- word.go
+++ word.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	var amount int
+	var createFile bool
 	rand.Seed(time.Now().UnixNano())
 
 	fmt.Println("ENTER THE NUMBER OF WORDS TO GENERATE:")
@@ -26,32 +27,56 @@ func main() {
 	fmt.Println("CREATE FILE? (true/false):")
 	fmt.Scan(&createFile)
 
-
 	charSets := buildCharSets(includeUppercase, includeLowercase, includeNumbers, includeSymbols)
 
+	var words []string
 	for i := 0; i < amount; i++ {
 		word := generateRandomWord(charSets)
-		fmt.Println("Generated Word:", word)
+		words = append(words, word)
+	}
+
+	if createFile {
+		file, err := os.Create("generated_words.txt")
+		if err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+		defer file.Close()
+
+		for _, word := range words {
+			_, err := file.WriteString(word + "\n")
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+				return
+			}
+		}
+
+		fmt.Println("Generated words have been written to 'generated_words.txt'.")
+	} else {
+		fmt.Println("Generated Words:")
+		for _, word := range words {
+			fmt.Println(word)
+		}
 	}
 }
 
 func buildCharSets(uppercase, lowercase, numbers, symbols bool) []string {
 	charSets := []string{}
-	if uppercase ==  {
+	if uppercase {
 		charSets = append(charSets, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	}
-	if lowercase{
+	if lowercase {
 		charSets = append(charSets, "abcdefghijklmnopqrstuvwxyz")
 	}
-	if numbers  {
+	if numbers {
 		charSets = append(charSets, "1234567890")
 	}
-	if symbols  {
+	if symbols {
 		charSets = append(charSets, "!@#$%&'()*+")
 	}
+
 	return charSets
 }
-
 
 func generateRandomWord(charSets []string) string {
 	var word string
@@ -60,5 +85,6 @@ func generateRandomWord(charSets []string) string {
 		randomChar := charSet[rand.Intn(len(charSet))]
 		word += string(randomChar)
 	}
+
 	return word
 }
